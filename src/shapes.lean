011119d383f39d4ba96fccd90d35a5d816d69529
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-03-28 22:44:49 -0700

    lemmas

--- src/shapes.lean
+++ src/shapes.lean
@@ -1,113 +1,45 @@
 import data.set
-import analysis.convex.basic
-import analysis.normed_space.inner_product
-import algebra.big_operators.basic
-
-namespace array
-
-def zip_with {α β γ : Type} {n} (x : array n α) (f : α → β → γ) (y : array n β) : array n γ := 
-  ⟨λ i, f (x.data i) (y.data i)⟩
-
-@[simp]
-lemma zip_with_read {α β γ : Type} {n : ℕ} (f : α → β → γ) (x : array n α) (y : array n β) {i : fin n} : 
-  (zip_with x f y).read i = f (x.read i) (y.read i) := rfl
-
-def sum {α} [add_monoid α] {n} (x : array n α) : α := x.foldl 0 (+)
-
-@[simp]
-lemma sum_zero {α} [add_monoid α] (x : array 0 α) : x.sum = 0 := rfl
-
-@[simp]
-lemma sum_succ {α} [add_monoid α] (n : ℕ) (x : array (n+1) α) : x.sum = (x.pop_back).sum + x.read (fin.last n) := 
-  by {
-    unfold sum,
-    unfold foldl,
-    unfold pop_back iterate d_array.iterate,
-    simp,
-  }
-
-end array
+import data.matrix.basic
 
 section point
 
 parameters {dim : nat}
 
-abbreviation point := array dim ℚ
+abbreviation point := fin dim → ℚ
+abbreviation point.dot : point → point → ℚ := λ x y, matrix.dot_product x y
 
-instance : has_add point := ⟨λ x y, x.zip_with (+) y⟩
-instance : has_zero point := ⟨⟨λ i, 0⟩⟩
-instance : has_neg point := ⟨λ p, p.map has_neg.neg⟩
-instance : add_comm_group point :=
-  {
-    add_assoc := by {
-      intros x y z,
-      unfold has_add.add,
-      ext,
-      simp,
-      apply add_assoc,
-    },
-    zero_add := λ x, array.ext (λ (i : fin dim), zero_add (x.data i)),
-    add_zero := λ x, array.ext (λ (i : fin dim), add_zero (x.data i)),
-    add_left_neg := λ x, array.ext (λ i, add_left_neg (x.data i)),
-    add_comm := λ x y, array.ext (λ i, add_comm (x.data i) (y.data i)),
-    .. point.has_add,
-    .. point.has_zero,
-    .. point.has_neg
-  }
+abbreviation direction := {p : point // p.dot p = 1}
 
-instance : has_scalar ℚ point := ⟨λ r p, ⟨λ i, r * p.data i⟩⟩ 
+abbreviation support_function := {s : direction → point // ∀ d₁ d₂ : direction, (s d₂).dot d₁ ≤ (s d₁).dot d₁}
 
-def point.semimodule.core  : semimodule.core ℚ point :=
-  {
-    add_smul := λ r₀ r₁ p, by {
-      ext,
-      unfold_projs,
-      simp,
-      unfold array.read d_array.read,
-      simp,
-      apply right_distrib,
-    },
-    smul_add := λ r₀ r₁ p, by {
-      ext,
-      unfold_projs,
-      simp,
-      unfold array.read d_array.read,
-      simp,
-      apply left_distrib,
-    },
-    mul_smul := λ r₀ r₁ p, by {
-      ext,
-      unfold array.read d_array.read,
-      unfold_projs,
-      simp,
-      apply mul_assoc,
-    },
-    one_smul := λ x, by {
-      ext,
-      unfold array.read d_array.read,
-      unfold_projs,
-      simp,
-      apply one_mul,
-    },
-    .. point.has_scalar
-  }
+def hull (s : support_function) : set point := (λ p : point, ∀ d : direction, d.val.dot d ≤ (s.val d).dot d)
 
-instance : vector_space ℚ point := semimodule.of_core point.semimodule.core
+instance : has_mem point support_function := ⟨λ p s, p ∈ hull s⟩ 
 
-abbreviation point.dot_aux (x y : point) : ℚ := (x.zip_with (*) y).sum
+def minkowski_diff (a b : set point) : set point :=
+  set.image2 has_sub.sub a b
 
-lemma dot_add_left (x y z : point) : point.dot_aux (x + y) z = (x.dot_aux z) + (y.dot_aux z) :=
+lemma minkowski_diff.def (a b : set point) : minkowski_diff a b = set.image2 has_sub.sub a b :=
+  rfl
+
+lemma minkowski_dif_contains_zero_iff_intersect (a b : set point) :
+  (0 : point) ∈ minkowski_diff a b ↔ (a ⊓ b).nonempty :=
   by {
-    unfold array.sum,
+    split; intros h,
+    {
+      obtain ⟨x, y, a_x, b_y, x_y⟩ := h,
+      use x,
+      split, {exact a_x},
+      rw (sub_eq_zero.mp x_y),
+      exact b_y,
+    },
+    {
+      obtain ⟨x, x_a, x_b⟩ := h,
+      use [x, x, x_a, x_b],
+      exact sub_self x
+    }
   }
 
-def dot : bilin_form ℚ point :=
-  {
-    bilin := point.dot_aux,
-    bilin_add_left := _,
-  }
-     
 
-end GJK
 
-end
+end point
