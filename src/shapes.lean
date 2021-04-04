44b0992c79cadb3569e5b10697bd605703674c52
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-04-03 18:53:08 -0700

    WIP

--- src/shapes.lean
+++ src/shapes.lean
@@ -5,14 +5,33 @@ import data.fincard
 import order.basic
 import tactic
 
+abbreviation nonempty_list (α) := {l : list α // ¬ l.is_nil}
+
 section point
 
-parameters {dim : nat}
+parameters (dim : nat)
 
 abbreviation point := fin dim → ℚ
+
+parameters {dim}
+
 def point.dot : point → point → ℚ := λ x y, matrix.dot_product x y
 
+lemma point.dot.def (x y : point) : x.dot y = matrix.dot_product x y := rfl
+
+lemma point.dot.neg_neg (x y : point) : x.dot y = (-x).dot (-y) :=
+  by {
+    simp_rw point.dot.def, 
+    rw matrix.dot_product_neg, 
+    rw matrix.dot_product_comm (-x), 
+    rw matrix.dot_product_neg,
+    rw neg_neg,
+    apply matrix.dot_product_comm,
+  }
+
 abbreviation direction := {p : point // p.dot p = (1 : ℚ)}
+
+instance : has_neg direction := ⟨λ ⟨p, magnitude_one⟩, ⟨-p, (point.dot.neg_neg p p).symm.trans magnitude_one⟩ ⟩
 
 abbreviation support_function := {s : direction → point // ∀ d₁ d₂ : direction, (s d₂).dot d₁ ≤ (s d₁).dot d₁}
 
@@ -45,6 +64,14 @@ lemma minkowski_dif_contains_zero_iff_intersect (a b : set point) :
       exact sub_self x
     }
   }
+
+def minkowski_diff_support (a b : support_function) : support_function :=
+  { 
+    val := λ dir, a.val dir - b.val (-dir),
+    property := sorry
+  }
+
+end point
 
 section get_maximum_by
 parameters {α β : Type} [linear_order β] (f : α → β)
@@ -157,13 +184,80 @@ lemma get_maximum_by.aux.in_l.aux (n : ℕ) : ∀ x (xs : list α),
 lemma get_maximum_by.aux.in_l (x xs) : (get_maximum_by.aux (f x, x) xs) ∈ ((x::xs).map (λ y : α, (f y, y))) :=
   by {apply get_maximum_by.aux.in_l.aux (xs.length), refl}
 
+lemma get_maximum_by.aux.is_maximal.aux (n : ℕ) : ∀ x (xs : list α), 
+  xs.length = n → 
+  ∀ y, y ∈ x::xs → f y ≤ f (get_maximum_by.aux (f x, x) xs).2 :=
+  by {
+    simp_rw get_maximum_by.aux.def,
+    induction n; intros x xs h y y_in_x_xs,
+    case zero {
+      rw list.length_eq_zero at h, rw h at *, clear h xs,
+      rw list.mem_singleton at y_in_x_xs, rw y_in_x_xs at *, clear y_in_x_xs y,
+      rw list.foldr_nil,
+    },
+    case succ {
+      cases xs with z zs,
+      case nil {
+        unfold list.length at *,
+        cases h,
+      },
+      case cons {
+        have n_ih' : y ∈ x :: zs → f y ≤ f (list.foldr get_maximum_by.aux_aux (f x, x) zs).snd,
+          {apply n_ih, unfold list.length at h, rw nat.add_one at h, injection h},
+        have y_in : y = z ∨ y ∈ x :: zs, 
+          { rcases y_in_x_xs with _ | _ | _, 
+            {right, left, exact y_in_x_xs}, 
+            {left, exact y_in_x_xs},
+            {right, right, exact y_in_x_xs},
+          },
+        rw list.foldr_cons,
+        have exists_fw_w : ∃ w, (list.foldr get_maximum_by.aux_aux (f x, x) zs) = (f w, w),
+          {
+            have H : (list.foldr get_maximum_by.aux_aux (f x, x) zs) ∈ list.map (λ t, (f t, t)) (x :: zs),
+            {
+              have H : get_maximum_by.aux (f x, x) zs ∈ (x::zs).map (λ y : α, (f y, y)) := 
+                by {apply get_maximum_by.aux.in_l},
+              rw get_maximum_by.aux.def at H,
+              exact H,
+            },
+            clear_except H,
+            cases H, 
+              {use x, apply H},
+              { 
+                have H : list.foldr get_maximum_by.aux_aux (f x, x) zs ∈ list.map (λ (t : α), (f t, t)) zs := H, 
+                rw list.mem_map at H,
+                rcases H with ⟨a, a_in_zs, f_a_a_eq⟩,
+                use a, symmetry, exact f_a_a_eq,
+              },
+          },
+        obtain ⟨w, fw_w⟩ := exists_fw_w,
+        rw fw_w,
+        cases y_in,
+        case inl {
+          transitivity (f z), {rw y_in},
+          apply get_maximum_by.aux_aux.le_fx,
+        },
+        case inr {
+          have HH := n_ih' y_in,
+          transitivity (f w),
+          rw fw_w at HH,
+          apply HH,
+          apply get_maximum_by.aux_aux.le_fy,
+        },
+      },
+    },
+  }
+
+lemma get_maximum_by.aux.is_maximal (x xs) (y) (y_in_l :y ∈ x::xs) : f y ≤ f (get_maximum_by.aux (f x, x) xs).2 :=
+  get_maximum_by.aux.is_maximal.aux (list.length xs) x xs (eq.refl _) y y_in_l
+
 def get_maximum_by : {l : list α // ¬ l.is_nil} → α
   | ⟨x::xs, _⟩ := (get_maximum_by.aux (f x, x) xs).2
   | ⟨[], p⟩ := by {exfalso, apply p, exact list.is_nil_iff_eq_nil.mpr rfl}
 
 lemma get_maximum_by.cons (x xs p) : get_maximum_by ⟨x :: xs, p⟩ = (get_maximum_by.aux (f x, x) xs).2 := rfl
 
-lemma get_maximum_by.in_l : ∀ l : {l : list α // ¬ l.is_nil}, get_maximum_by l ∈ l.val
+lemma get_maximum_by.in_l : ∀ l : nonempty_list α, get_maximum_by l ∈ l.val
   | ⟨x::xs, is_not_nil⟩ := by {
       rw get_maximum_by.cons, 
       unfold subtype.val,
@@ -180,35 +274,70 @@ lemma get_maximum_by.in_l : ∀ l : {l : list α // ¬ l.is_nil}, get_maximum_by
       unfold prod.snd,
       {right, exact z_in},
     }
-  | ⟨[], p⟩ := by {exfalso, apply p, exact list.is_nil_iff_eq_nil.mpr rfl}
+  | ⟨[], p⟩ := false.rec _ (p (list.is_nil_iff_eq_nil.mpr rfl))
+
+lemma get_maximum_by.is_maximal : ∀ (l : nonempty_list α) (x : α), (x ∈ l.val) → f x ≤ f (get_maximum_by l)
+  | ⟨x::xs, is_not_nil⟩ := by {
+      rw get_maximum_by.cons,
+      unfold subtype.val,
+      exact get_maximum_by.aux.is_maximal x xs
+    }
+
+  | ⟨[], p⟩ := λ _, false.rec _ (p (list.is_nil_iff_eq_nil.mpr rfl))
 
 end get_maximum_by
 
-def polygon.val (points : {l : list point // ¬ l.is_nil}) (dir : direction) : point :=
-  get_maximum_by (λ p : point, p.dot dir.val) points
+def polygon.val {n} (points : nonempty_list (point n)) (dir : direction) : point n :=
+  get_maximum_by (λ p : point n, p.dot dir.val) points
 
-lemma polygon.property {dim : ℕ} (points : list point) (d₁ d₂ : direction) :
+lemma polygon.val.def {n} (points : nonempty_list (point n)) (dir) 
+  : polygon.val points dir = get_maximum_by (λ p : point n, p.dot dir.val) points :=
+  rfl
+
+lemma polygon.property {n : ℕ} (points : nonempty_list (point n)) (d₁ d₂ : direction) :
     (polygon.val points d₂).dot ↑d₁ ≤ (polygon.val points d₁).dot ↑d₁ :=
   by {
-
+    simp_rw polygon.val.def,
+    apply get_maximum_by.is_maximal (λ p : (point n), p.dot ↑d₁ ) points _,
+    apply get_maximum_by.in_l,
   }
 
-def support_function.polygon (points : list point) : support_function :=
-  
+def support_function.polygon {n} (points : nonempty_list (point n)) : support_function :=
   {
     val := polygon.val points,
-    property := by {extract_goal, }
+    property := polygon.property points
   }
 
--- "canon" because you can add (some) points on bounding faces of a simplex to the support function and not change the hull. So the hull of every `s` such that `s.is_canon_simplex` is a simplex, and every simplex has such a support_function, but there are `s` such that `¬ s.is_canon_simplex` and `s.hull` is a simplex.
+-- "canon" because you can add (some) points on bounding faces of a simplex to the support function and not change the hull. So the hull of every `s` such that `s.is_canon_simplex` is a simplex, and every simplex has such a support_function, but there are `s` such that `s.hull` is a simplex `¬ s.is_canon_simplex` and
 
-def support_function.is_canon_simplex (s : support_function) : Prop := enat.card (s.range) ≤ dim + 1
+def support_function.is_canon_simplex {dim} (s : @support_function dim) : Prop := enat.card (s.range) ≤ dim + 1
 
-lemma aux (a b : support_function) (s : support_function) (p : point) 
-  (s_supports : s.hull = minkowski_diff a.hull b.hull)  (p_in_s : p ∈ s) :
-  ∃ s' : support_function, s'.is_canon_simplex ∧ s'.range ⊆ s.range ∧ p ∈ s' :=
-  by {
+section GJK
 
-  }
+parameters {dim : ℕ} (a b : @support_function dim)
 
-end point
+namespace GJK
+
+def getNextPoint (dir) : point dim := (minkowski_diff_support a b).val dir
+
+def nearestSimplex (simplex : list (point dim)) : list (point dim) × @direction dim × bool := sorry
+
+def loop : @direction dim → list (point dim) → bool
+  | dir simplex :=
+    let a : point dim := getNextPoint dir in
+    if a.dot dir.val < 0
+      then ff
+      else match nearestSimplex simplex with
+        | (_, _, tt) := tt
+        | (simplex', dir', ff) := loop dir simplex
+      end
+  using_well_founded _
+
+def start (dir : @direction dim) : bool := 
+  let nextPoint := getNextPoint dir in
+  let simplex := [nextPoint] in
+  loop (-dir) simplex
+
+def GJK.aux  : (0 : point _) ∈ minkowski_diff a.hull b.hull
+
+def GJK {dim} (a b : @support_function dim) : (∃ p : point dim, p ∈ a ∧ p ∈ b) ∨ ¬ (∃ p : point dim, p ∈ a ∧ p ∈ b) :=
